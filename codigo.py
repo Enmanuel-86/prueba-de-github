400c461fa46d3c8aa97fbe70fb5b404773f2bd75
Enmanuel-86 <enmanuelgarcia648@gmail.com>
2025-08-27 06:17:18 -0400

    nueva modificacion en la rama-modificar

--- codigo.py
+++ codigo.py
@@ -1,27 +1,3 @@
-usuarios = ["Jose", "Manuel", "lopez"]
+print("suma de dos numeros es:  ", 3 + 5)
 
-def revisar(usuario, contra):
-    
-    
-    if usuario in usuarios and contra == "123":
-        print(f"bienvenido {usuario}")
-        
-    else:
-        
-        print(f"el usuario {usuario} no esta registrado")
-        
-    
-
-print("--------------------------------")
-print("-------------Login-------------")
-print("--------------------------------")
-
-
-usuario = input("Usuario: ")
-contra = input("Contraseña: ")
-
-
-revisar(usuario, contra)
-
-print("hola como estas, primer cambio en la rama master")
-
+print("nueva funcion en esta rama rama-modificar")
